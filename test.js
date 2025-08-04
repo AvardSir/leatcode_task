5426c7fe3745b5d2efaf876800e1277dd81d4153
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-08-04 20:45:58 +0300

    l try third try to solve myself LRU Cache

--- test.js
+++ test.js
@@ -1,46 +1,8 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     constructor(val = 0, next = null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
+let a=new Map()
 
-class Solution {
-    /**
-     * @param {ListNode} head
-     * @param {number} n
-     * @return {ListNode}
-     */
-    removeNthFromEnd(head, n) { }
-}
-
-class ListNode {
-    constructor(val = 0, next = null) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
-let a1 = new ListNode(1)
-let a2 = new ListNode(2, a1)
-let a3 = new ListNode(3, a2)
-let step = a3
-let arr = [a1, a2, a3]
-arr[1].next = null
-while (step) {
-    console.log('step::: ', step);
-    step = step.next
-
-}
-
-
-// 
-a = new Solution()
-// 
-
-
-nums2 = [2, 3, 5, 6]
-console.log('Задача ответ::: ', a.findMedianSortedArrays(nums1, nums2));
+a.set(32,23)
+a.set(51,15)
+console.log('a::: ', a);
+a.get(32)
+console.log('a.get(32)::: ', a.get(32));
+let pas=1
