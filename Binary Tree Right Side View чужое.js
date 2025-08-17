35e2edf7c9fe793c5b7222fc56754d6d4216a820
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-08-17 20:40:33 +0300

    Count Good Nodes in Binary Tree myself!

---
+++ Binary Tree Right Side View чужое.js
@@ -0,0 +1,104 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root) {
+        if (!root) {
+            return []
+        }
+        let ans = [root.val]
+        // let prev
+        let stack = [root]
+        while (stack.length) {
+            let n = stack.length
+            // console.log('stack::: ', stack);
+
+            for (let i = 0; i < n; i++) {
+                let curNode = stack.shift()
+
+                if (curNode.left) {
+                    stack.push(curNode.left)
+                    // console.log('stack.push::: ');
+                }
+                if (curNode.right) {
+                    stack.push(curNode.right)
+                    // console.log('stack.push::: ');
+
+                }
+            }
+            // console.log('stack::: ', stack);
+            if (stack[stack.length - 1]) {
+                ans.push(stack[stack.length - 1].val)
+
+            }
+            else {
+                break
+            }
+            // stack.shift()
+
+        }
+        return ans
+    }
+}
+
+
+
+
+class TreeNode {
+    constructor(val = 0, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+function arrayToTree(arr) {
+    if (arr.length === 0) return null;
+
+    // Создаём корень из первого элемента
+    const root = new TreeNode(arr[0]);
+    const queue = [root]; // Очередь для обработки узлов
+    let i = 1; // Индекс для прохода по массиву
+
+    while (queue.length > 0 && i < arr.length) {
+        const currentNode = queue.shift(); // Берем первый узел из очереди
+
+        // Левый потомок
+        if (i < arr.length && arr[i] !== null) {
+            currentNode.left = new TreeNode(arr[i]);
+            queue.push(currentNode.left);
+        }
+        i++;
+
+        // Правый потомок
+        if (i < arr.length && arr[i] !== null) {
+            currentNode.right = new TreeNode(arr[i]);
+            queue.push(currentNode.right);
+        }
+        i++;
+    }
+
+    return root;
+}
+let a = new Solution()
+
+root = [4, 2, 7, 1, 3, 6, 9]
+root = arrayToTree(root)
+
+root = [1, 2, 3]
+root = arrayToTree(root)
+let root2 = [1, 2, 7, 1, 3, 6, 9]
+root2 = arrayToTree(root2)
+
+console.log('Ответ::: ', a.isSubtree(root, root2));
