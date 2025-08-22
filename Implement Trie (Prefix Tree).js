10c254b39282414cd604d1eaf2a7df1f9ae5dbd0
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-08-22 12:30:14 +0300

    start wok on Implement Trie

---
+++ Implement Trie (Prefix Tree).js
@@ -0,0 +1,26 @@
+class PrefixTree {
+    constructor() {}
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {}
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {}
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {}
+}
+
+
+// deserialize(root3)
+console.log('Ответ::: ', a.serialize(root));
+console.log('deserialize(root3)::: ', a.deserialize(root3));
