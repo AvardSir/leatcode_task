f23a87c312be5175efc24b5e85144bcfde3d872d
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-10-25 15:04:44 +0300

    Max Area of Island with prev idea optimization by myself

--- Max Area of Island.js
+++ Max Area of Island.js
@@ -1,51 +1,39 @@
 class Solution {
     /**
-     * @param {character[][]} grid
+     * @param {number[][]} grid
      * @return {number}
      */
-    numIslands(grid) {
-
-        let memo = Array.from({ length: grid.length }, v => Array.from({ length: grid[0].length }, v => false))
-
-        for (let i = 0; i < grid.length; i++) {
-            // const element = array[i];
-            for (let j = 0; j < grid[0].length; j++) {
-                const element = grid[i][j];
-                grid[i][j] = Number(grid[i][j])
-
-            }
-
-        }
-        let check1 = 1
+    maxAreaOfIsland(grid) {
+        let curSize
+        let maxSize = 0
 
         function dfs(i, j) {
-            if (i < 0 || j < 0 || i > grid.length - 1 || j > grid[0].length - 1 || grid[i][j] == 0 || memo[i][j] == true) {
+
+            if (i < 0 || j < 0 || i > grid.length - 1 || j > grid[0].length - 1 || grid[i][j] == 0) {
                 return
             }
-            memo[i][j] = 1
+
+            grid[i][j] = 0
+            curSize++
             dfs(i + 1, j)
             dfs(i, j + 1)
             dfs(i - 1, j)
             dfs(i, j - 1)
-            // memo[i][j] = 0
-
         }
-
-        let ans = 0
         for (let i = 0; i < grid.length; i++) {
             // const element = array[i];
             for (let j = 0; j < grid[0].length; j++) {
                 const element = grid[i][j];
-
-                if (!memo[i][j] && grid[i][j] == 1) {
-                    ans++
+                if (element == 1) {
+                    curSize = 0
                     dfs(i, j)
+                    maxSize = Math.max(maxSize, curSize)
                 }
 
             }
-
         }
-        return ans
+
+        return maxSize
     }
 }
 
@@ -53,10 +41,11 @@ class Solution {
 let sol = new Solution()
 
 grid = [
-    ["0", "1", "1", "1", "0"],
-    ["0", "1", "0", "1", "0"],
-    ["1", "1", "0", "0", "0"],
-    ["0", "0", "0", "0", "0"]
+    [0, 1, 1, 0, 1],
+    [1, 0, 1, 0, 1],
+    [0, 1, 1, 0, 1],
+    [0, 1, 0, 0, 1]
 ]
 
-console.log(' ::: ', sol.numIslands(grid));
+
+console.log(' ::: ', sol.maxAreaOfIsland(grid));
