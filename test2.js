1aed962ab68a0a0008af49dedf7b3c470a6c63af
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-11-02 16:16:21 +0300

    Course Schedule by myself and also add tiny optimization from tutorial

--- test2.js
+++ test2.js
@@ -1,10 +1,3 @@
-let map = new Map()
+let neSet=new Set()
 
-map.set([1, 2])
-
-if (map.has([1, 2])) {
-    console.log('1111::: ', 1111);
-}
-else{
-    console.log('22222::: ', 22222);
-}
+neSet.delete()
