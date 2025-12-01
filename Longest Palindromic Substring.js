a7b95984bff605c43f834aedfa543fad9b41adea
AvardSir <114823780+AvardSir@users.noreply.github.com>
2025-12-01 13:39:50 +0300

    Longest Palindromic Substring by myself prety optimal

---
+++ Longest Palindromic Substring.js
@@ -0,0 +1,90 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+        function isPolid(curStr) {
+            // let ar=[1,2]
+            // // ar.length
+            let l = 0
+            let r = curStr.length - 1
+
+            while (l < r) {
+                if (curStr[l] != curStr[r]) {
+                    return false
+                }
+                l++
+                r--
+            }
+            return true
+
+        }
+        // let curStr = 'abab'
+        // isPolid(curStr)
+        // console.log('isPolid(curStr)::: ', isPolid(curStr));
+
+        let maxStr = ''
+        for (let i = 0; i < s.length; i++) {
+            const element = s[i];
+
+            let diap = 0
+            let j = i
+            while (true) {
+                let l = j - diap
+                let r = i + diap
+                if (l < 0 || r > s.length - 1) {
+                    break
+                }
+
+                let curStr = s.slice(l, r + 1)
+                if (!isPolid(curStr)) {
+                    break
+                }
+                if (curStr.length > maxStr.length) {
+                    maxStr = curStr
+                }
+
+                diap++
+            }
+            j = i - 1
+            if (j < 0) {
+                continue
+            }
+            
+            diap = 0
+            while (true) {
+                let l = j - diap
+                let r = i + diap
+                if (l < 0 || r > s.length - 1) {
+                    break
+                }
+
+                let curStr = s.slice(l, r + 1)
+                if (!isPolid(curStr)) {
+                    break
+                }
+                if (curStr.length > maxStr.length) {
+                    maxStr = curStr
+                }
+
+                diap++
+            }
+
+        }
+        return maxStr
+        // todo отладить я даж не запускал
+    }
+}
+
+
+
+const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
+let sol = new Solution()
+
+
+
+s = "ababd"
+s = "abbc"
+
+console.log(' ::: ', sol.longestPalindrome(s));
